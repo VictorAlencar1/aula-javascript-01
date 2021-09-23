1edd46bdf7f06af710f9f66d8bf1f25aaf1d5f12
Victor Alencar <victoralencarferraz@hotmail.com>
2021-09-23 14:44:06 -0300

    add file

---
+++ script.js
@@ -0,0 +1,23 @@
+var nome = "Victor";
+
+console.log("Meu nome é " + nome);
+
+var idade = "20 anos";
+
+console.log("Tenho " + idade);
+
+let time = "Náutico";
+
+console.log("Eu torço pro " + time);
+
+let estadio = "Aflitos";
+
+console.log("O náutico joga nos " + estadio);
+
+const fundacao = "1901";
+
+console.log("O Náutico foi fundado no ano de " + fundacao);
+
+const titulo = "2021";
+
+console.log("O náutico foi campeão pernambucano em " + titulo);
